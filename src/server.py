b3b4c2c01cac94eec9390380ad821d54faeaaac8
Nicholas Sokolov <sokolov.nicholas@gmail.com>
2019-02-26 19:03:35 +0700

    Switch to non-block mode

--- src/server.py
+++ src/server.py
@@ -1,5 +1,9 @@
-import socket
 import logging
+import selectors
+import socket
+import types
+
+select = selectors.DefaultSelector()
 
 logging.basicConfig(format='[%(asctime)s] %(levelname).1s %(message)s',
                     datefmt='%Y.%m.%d %H:%M:%S',
@@ -24,14 +28,14 @@ ERRORS = {
 
 
 class Server:
-
     SUPPORTED_METHODS = [GET, HEAD]
 
     def __init__(self, host, port, timeout=10, bind_and_activate=True):
         self.host = host
         self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.timeout = timeout
         self.error = OK
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if bind_and_activate:
             self.server_bind()
             self.server_activate()
@@ -39,21 +43,46 @@ class Server:
     def server_bind(self):
         self.socket.bind((self.host, self.port))
 
-    def server_activate(self):
-        self.socket.listen(5)
-        conn, addr = self.socket.accept()
-        # with conn:
-        #     server_logger.info('Received connection from {}'.format(addr))
+    def server_activate(self, poll_interval=0.5):
+        self.socket.listen(0)
+        server_logger.info("Listener was started on {} {}".format(self.host, self.port))
+        self.socket.setblocking(False)
+        select.register(self.socket, selectors.EVENT_READ)
+
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            self.data_process(data)
+            events = select.select(poll_interval)
+            for key, mask in events:
+                if key.data is None:
+                    self.accept_wrapper(key.fileobj)
+                else:
+                    self.service_connection(key, mask)
+
+    @staticmethod
+    def accept_wrapper(sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        server_logger.info('Accepted connection from {}'.format(addr))
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        select.register(conn, events, data=data)
+
+    @staticmethod
+    def service_connection(key, mask):
+        sock = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)  # Should be ready to read
+            if recv_data:
+                data.outb += recv_data
+            else:
+                server_logger.info('Closing connection to {}'.format(data.addr))
+                select.unregister(sock)
+                sock.close()
+        if mask & selectors.EVENT_WRITE:
+            if data.outb:
+                server_logger.info('Echoing {} to {}'.format(repr(data.outb), data.addr))
+                sent = sock.send(data.outb)  # Should be ready to write
+                data.outb = data.outb[sent:]
 
     def close(self):
         self.socket.close()
-
-    def data_process(self, data):
-        method, path, protocol = data.decode().split('\r\n')[0].split()
-        if method not in self.SUPPORTED_METHODS:
-            self.error = INVALID_REQUEST
