eed07aafc623c0f67ab821b9decb3999b653ccab
Nicholas Sokolov <sokolov.nicholas@gmail.com>
2019-02-26 22:25:40 +0700

    Added some important logic

--- src/server.py
+++ src/server.py
@@ -1,6 +1,8 @@
+import email.utils
 import logging
 import selectors
 import socket
+import time
 import types
 
 select = selectors.DefaultSelector()
@@ -37,6 +39,8 @@ class Server:
         self.timeout = timeout
         self.error = OK
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+        self._headers_buffer = []
         if bind_and_activate:
             self.server_bind()
             self.server_activate()
@@ -67,23 +71,50 @@ class Server:
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
         select.register(conn, events, data=data)
 
-    @staticmethod
-    def service_connection(key, mask):
+    def service_connection(self, key, mask):
         sock = key.fileobj
         data = key.data
         if mask & selectors.EVENT_READ:
-            recv_data = sock.recv(1024)  # Should be ready to read
-            if recv_data:
-                data.outb += recv_data
-            else:
-                server_logger.info('Closing connection to {}'.format(data.addr))
-                select.unregister(sock)
-                sock.close()
+            try:
+                recv_data = sock.recv(1024)  # Should be ready to read
+                if recv_data:
+                    data.outb += recv_data
+                else:
+                    server_logger.info('Closing connection to {}'.format(data.addr))
+                    select.unregister(sock)
+                    sock.close()
+            except (ConnectionAbortedError, ConnectionResetError) as err:
+                server_logger.debug(str(err))
+                pass
         if mask & selectors.EVENT_WRITE:
             if data.outb:
                 server_logger.info('Echoing {} to {}'.format(repr(data.outb), data.addr))
-                sent = sock.send(data.outb)  # Should be ready to write
-                data.outb = data.outb[sent:]
+                self.response(data.outb)
+
+    def response(self, byte_data):
+        # TODO: Headers: Connection, Content‑Length, Content‑Type
+        # TODO: Content‑Type for .html, .css, .js, .jpg, .jpeg, .png, .gif, .swf
+        self.send_header('Server', 'MyCustomServer 0.1')
+        self.send_header('Date', email.utils.formatdate(time.time(), usegmt=True))
+
+        data_list = byte_data.decode().split('\r\n')
+        if not data_list:
+            raise Exception('No list!!!!')
+        elif not data_list[0].strip():
+            return
+
+        method, url, version = data_list[0].split()
+
+        if method not in self.SUPPORTED_METHODS:
+            raise Exception('No such method!!!!')
+        if url.endswith('/'):
+            pass
+        else:
+            pass
+
+    def send_header(self, keyword, value):
+        """Send a MIME header to the headers buffer."""
+        self._headers_buffer.append("{}: {}\r\n".format(keyword, value))
 
     def close(self):
         self.socket.close()
